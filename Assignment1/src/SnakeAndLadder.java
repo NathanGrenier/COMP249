ab3aab74938ca89ee53b1cf55880058a41fabf07
Nathan Grenier <nathangrenier01@gmail.com>
2023-01-14 17:54:07 -0500

    Initial commit

---
+++ Assignment1/src/SnakeAndLadder.java
@@ -0,0 +1,15 @@
+ // -----------------------------------------------------
+ // Assignment 1
+ // Question: (include question/part number, if applicable)
+ // Written by: Nathan Grenier, 40250986
+ // -----------------------------------------------------
+import java.util.Scanner;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        System.out.println(num);
+    }
+}
