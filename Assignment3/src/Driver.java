7786efdd4ecec773f0453cc8d0286622f4fda0f8
Nathan Grenier <nathangrenier01@gmail.com>
2023-03-20 02:36:04 -0400

    Started the assignment. No real progress

--- Assignment3/src/Driver.java
+++ Assignment3/src/Driver.java
@@ -1,5 +1,102 @@
+ // -----------------------------------------------------
+ // Written by: Nathan Grenier, 40250986
+ // COMP249
+ // Assignment #3
+ // Question: (include question/part number, if applicable)
+ // Due: March 24, 2023
+ // -----------------------------------------------------
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Driver {
-    public static void main(String[] args) {
+    private static final String inputFileName = "Assignment3/Files/Read/part1_input_file_names.txt";
+    private static final String outputFileName = "Assignment3/Files/Read/part1_output_file_names.txt";
+
+    /**
+     * Opens a file to read data from.
+     * 
+     * @param name
+     * @return Scanner linked to input file.
+     */
+    private static Scanner openFile(String name) {
+        Scanner input = null;
+        try {
+            input = new Scanner(new FileInputStream(name));
+        } catch (FileNotFoundException e) {
+            System.out.println("File: " + name + " does not exist.");
+        }
+        return input;
+    }
+
+    /**
+     * Opens an output file for modification.
+     * If the output file dosen't exist, it will create one.
+     * 
+     * @param name
+     * @param append
+     * @return PrintWriter object linked to output file.
+     */
+    private static PrintWriter openOutputFile(String name, boolean append) {
+        PrintWriter output = null;
+        try {
+            output = new PrintWriter(new FileOutputStream(name));
+        } catch (FileNotFoundException e) {
+            System.out.println("File: " + name + "couldn't be found or created");
+        }
+        return output;
+    }
+
+    private static String[] parseBooks() {
+
+    }
+
+    
+    
+    
+    private static void do_part1() {
+        // Open file containing the name of the files that contain the books and their information
+        Scanner inputFileNameReader = openFile(inputFileName);
+        if (inputFileNameReader == null) {
+            System.out.println("The file containing the names of the files that contain the recorded books does not exist. Terminating program.");
+            System.exit(0);
+        }
         
+        // Parse the file containing file names and store the names in an array
+        int bookFileCount = Integer.parseInt(inputFileNameReader.nextLine());   // The first line will always be a number
+        String[] bookFiles = new String[bookFileCount];
+        for (int i=0; i < bookFileCount; i++) {
+            bookFiles[i] = inputFileNameReader.nextLine();
+        }
+
+        // Parse file containing the names of the genres and file output names
+        Scanner outputFileNameReader = openFile(outputFileName);
+        if (outputFileNameReader == null) {
+            System.out.println("The file containing the names of the genres does not exist. Terminating program.");
+            System.exit(0);
+        }
+        // Parse the file containing the genres and store them in an array
+        int genreCount = Integer.parseInt(outputFileNameReader.nextLine());
+        String[][] outputFiles = new String[genreCount][3]; // Format: [Name, Genre, File Name]
+        for (int i=0; i < genreCount; i++) {
+            outputFiles[i] = outputFileNameReader.nextLine().split(",");
+        }
+
+
+    }
+    
+    
+    public static void main(String[] args) {
+        do_part1(); // validating syntax, partition book records based on genre.
+        
+        /* 
+        do_part2(); // validating semantics, read the genre files each into arrays of Book objects,
+                    // then serialize the arrays of Book objects each into binary files.
+        do_part3(); // reading the binary files, deserialize the array objects in each file, and
+                    // then provide an interacive program to allow the user to navigate the arrays.
+        */
     }
 }
---
+++ Assignment3/src/Exceptions/MissingFieldException.java
@@ -0,0 +1,5 @@
+package Exceptions;
+
+public class MissingFieldException extends Exception {
+    
+}
